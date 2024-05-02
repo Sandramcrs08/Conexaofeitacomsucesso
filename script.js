eb01b0530af4d01bbb2fd98bb1c169d0fbbcb817
sandramcrs08 <sandramcrs@hotmail.com>
2024-05-02 10:36:43 -0300

    Atualizando arquivos

---
+++ script.js
@@ -0,0 +1,8 @@
+function imprimirMensagem(){
+    console.log("Conexão feita com sucesso!");
+} 
+
+// Chama a função para imprimir a mensagem três vezes
+imprimirMensagem();
+imprimirMensagem();
+imprimirMensagem();
